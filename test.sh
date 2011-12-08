71aae60365ba1fc818bcb1a7a76c7c4d25423a1d
catseye <cpressey@gmail.com>
2011-12-08 08:51:01 -0600

    Improve test harness; fix up p-normalizer and test suite for it.

--- test.sh
+++ test.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+# run Falderal tests
 cd src && falderal test tests.falderal
+cd ..
+
+# sanity-test scheme.sh and pixley.sh
+cat >expected.sexp <<EOF
+(ten (eight nine) seven six five four three two one)
+EOF
+script/scheme.sh eg/reverse.pix eg/some-list.sexp > out.sexp
+diff -u expected.sexp out.sexp
+script/pixley.sh eg/reverse.pix eg/some-list.sexp > out.sexp
+diff -u expected.sexp out.sexp
+rm -f expected.sexp out.sexp
+
+# test p-normalization
+cd eg && falderal test p-normal.falderal
